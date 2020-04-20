0cf82637038f56f07e21c4022505266cf959bc7c
SyedAhkam <smahkam57@gmail.com>
2020-04-20 23:10:52 +0530

    Added custom prefixes for guilds.

--- bot.py
+++ bot.py
@@ -4,12 +4,27 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 import os
+import json
+
+from pymongo import MongoClient
+mongo_client = MongoClient(
+    'mongodb+srv://owner:WNmlbnAiuBU5oQxn@cluster0-cppwj.gcp.mongodb.net/test?retryWrites=true&w=majority')
+db = mongo_client.jeju
+guilds_collection = db.guilds
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 PREFIX = os.getenv('DISCORD_PREFIX')
 
-bot = commands.Bot(command_prefix=PREFIX)
+def get_prefix(bot, message):
+    if message.guild:
+        guild = guilds_collection.find_one(filter={"guild_id": message.guild.id})
+        return guild['guild_prefix']
+
+    else:
+        return "+"
+
+bot = commands.Bot(command_prefix=get_prefix)
 
 @bot.event
 async def on_ready():
@@ -20,19 +35,32 @@ async def on_ready():
     print('Status changed')
 
 @bot.event
-async def on_error(event, error, *args, **kwargs):
-    with open('err.log', 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
-        else:
-            raise
-    print('An error occured')
-    print(error)
+async def on_guild_join(guild):
+
+        post = {
+            "guild_id": guild.id,
+            "guild_name": guild.name,
+            "guild_prefix": "+",
+            "guild_region": guild.region,
+            "guild_icon": guild.icon,
+            "guild_owner_id": guild.owner_id,
+            "members": len(guild.members),
+            "guild_description": guild.description,
+            "guild_verification": guild.verification_level,
+            "guild_features": guild.features
+        }
+        guilds_collection.insert_one(post)
+
+@bot.event
+async def on_guild_remove(guild):
+    guilds_collection.delete_one(filter={"guild_id": guild.id})
 
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('Looks like you don\'t have permission to access this command.')
+    else:
+        raise error
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
--- cogs/utilities.py
+++ cogs/utilities.py
@@ -1,6 +1,14 @@
 import discord
 from discord.ext import commands
 
+import json
+
+from pymongo import MongoClient
+mongo_client = MongoClient(
+    'mongodb+srv://owner:WNmlbnAiuBU5oQxn@cluster0-cppwj.gcp.mongodb.net/test?retryWrites=true&w=majority')
+db = mongo_client.jeju
+guilds_collection = db.guilds
+
 class Utilities(commands.Cog):
 
     def __init__(self, bot):
@@ -11,6 +19,18 @@ class Utilities(commands.Cog):
         #invite_url = discord.utils.oauth_url(client_id='699595477934538782', permissions='administrator', guild=None)
         invite_url = 'https://discordapp.com/api/oauth2/authorize?client_id=699595477934538782&permissions=8&scope=bot'
         await ctx.send(f'Invite me using this link:\n{invite_url}')
+
+    @commands.command(name='change_prefix', help='Change the bot\'s prefix.')
+    @commands.has_permissions(administrator=True)
+    async def change_prefix(self, ctx, prefix=None):
+        if not prefix:
+            await ctx.send('Please specify a prefix.')
+            return
+
+        guilds_collection.find_one_and_update({"guild_id": ctx.guild.id}, {'$set': {"guild_prefix": prefix}})
+
+        await ctx.send(f'Prefix set to ``{prefix}``')
+
 
 def setup(bot):
     bot.add_cog(Utilities(bot))

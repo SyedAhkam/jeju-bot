a4c62ecf126f67dcad04f72804205c450e5fc756
SyedAhkam <smahkam57@gmail.com>
2020-04-26 18:14:42 +0530

    Added config.py and made two commands in it.

---
+++ cogs/config.py
@@ -0,0 +1,47 @@
+from discord.ext import commands
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+import os
+
+load_dotenv()
+MONGO_URI = os.getenv('MONGODB_URI')
+
+mongo_client = MongoClient(MONGO_URI)
+db = mongo_client.jeju
+guilds_collection = db.guilds
+
+
+class Config(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='set_mod_role', help='Set a role to be used as a ModRole.')
+    @commands.has_permissions(administrator=True)
+    async def set_mod_role(self, ctx, role: commands.RoleConverter=None):
+        if not role:
+            await ctx.send('Please provide a role to be set as a ModRole.')
+            return
+
+        if role.name == '@everyone':
+            await ctx.send('I highly discourage making everyone a Mod.')
+            return
+
+        guilds_collection.find_one_and_update({"guild_id": ctx.guild.id}, {'$set': {"mod_role": role.id}})    
+
+        await ctx.send(f'Role ``{role.name}`` with ID: ``{role.id}`` has been set as a ModRole successfully.')
+
+    @commands.command(name='set_modlog_channel', help='Set a channel to be used for ModLogs.')
+    @commands.has_permissions(administrator=True)
+    async def set_modlog_channel(self, ctx, channel: commands.TextChannelConverter=None):
+        if not channel:
+            await ctx.send('Please provide a channel to set as a ModLogs Channel.')
+            return
+        
+        guilds_collection.find_one_and_update({"guild_id": ctx.guild.id}, {'$set': {"modlog_channel": channel.id}})
+        await ctx.send(f'Channel ``{channel.name}`` with ID: ``{channel.id}`` has been set up as a ModLogs channel successfully.')
+
+
+def setup(bot):
+    bot.add_cog(Config(bot))

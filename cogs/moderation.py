f29c0967a0aa9fc8397d7561a544a8e67712e3a4
SyedAhkam <smahkam57@gmail.com>
2020-04-26 18:18:16 +0530

    Added a new check and modified a bit for logs.

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from pymongo import MongoClient
 from dotenv import load_dotenv
 
 import discord
@@ -6,10 +7,24 @@ import os
 import datetime
 
 load_dotenv()
-MODLOGS = int(os.getenv('MODLOGS_CHANNEL'))
-SAYROLE = int(os.getenv('SAY_ROLE_ID'))
-DMROLE = int(os.getenv('DM_ROLE_ID'))
+MONGO_URI = os.getenv('MONGODB_URI')
+mongo_client = MongoClient(MONGO_URI)
+db = mongo_client.jeju
+guilds_collection = db.guilds
 
+def is_mod():
+    def predicate(ctx):
+        guild = guilds_collection.find_one(filter={"guild_id": ctx.guild.id})
+        mod_role = guild['mod_role']
+
+        if not mod_role:
+            ctx.channel.send('Please setup a mod_role using the command ``set_mod_role``')
+            return
+
+        check = discord.utils.find(lambda r: r.id == mod_role, ctx.author.roles)
+        if check:
+            return True
+    return commands.check(predicate)
 
 class Moderation(commands.Cog):
 
@@ -24,8 +39,22 @@ class Moderation(commands.Cog):
             await ctx.send('Please provide a user.')
             return
 
-        if not reason:
-            await ctx.send('Please provide a reason.')
+        if user == ctx.author:
+            await ctx.send('You can\'t kick yourself.')
+            return
+
+        if user.id == ctx.guild.owner_id:
+            await ctx.send('You can\'t kick the server owner.')
+            return
+
+        guild = guilds_collection.find_one(filter={"guild_id": ctx.guild.id})
+        modlog_channel = guild['modlog_channel']
+
+        if not modlog_channel:
+            await ctx.send('ModLogs channel not found, Set it up using ``set_modlog_channel``')
+
+            await user.kick(reason=reason)
+            await ctx.send(f'User {user.name} has been kicked successfully.')
             return
 
         kick_embed = discord.Embed(title='Kicked', description='A user have been kicked.', color=0xFFFFFF, timestamp=datetime.datetime.utcnow())
@@ -38,25 +67,38 @@ class Moderation(commands.Cog):
 
         kick_embed.set_footer(text=f'UserID: {user.id}')
 
-        channel = ctx.bot.get_channel(MODLOGS)
-        await channel.send(embed=kick_embed)
+        channel = ctx.bot.get_channel(modlog_channel)
 
         await user.kick(reason=reason)
+
+        await channel.send(embed=kick_embed)
         await ctx.send(f'User {user.name} has been kicked successfully.')
 
     @commands.command(name='ban', help='Bans a specified user.')
     @commands.has_permissions(ban_members=True)
-    async def ban(self, ctx, id: int, *, reason):
+    async def ban(self, ctx, user: commands.MemberConverter=None, *, reason=None):
 
-        if not id:
-            await ctx.send('Please provide a user\'s id.')
+        if not user:
+            await ctx.send('Please provide a user.')
             return
 
-        if not reason:
-            await ctx.send('Please provide a reason.')
+        if user == ctx.author:
+            await ctx.send('You can\'t ban yourself.')
             return
 
-        user = ctx.bot.get_user(id)
+        if user.id == ctx.guild.owner_id:
+            await ctx.send('You can\'t ban the server owner.')
+            return
+
+        guild = guilds_collection.find_one(filter={"guild_id": ctx.guild.id})
+        modlog_channel = guild['modlog_channel']
+
+        if not modlog_channel:
+            await ctx.send('ModLogs channel not found, Set it up using ``set_modlog_channel``')
+
+            await user.ban(reason=reason)
+            await ctx.send(f'User {user.name} has been banned successfully.')
+            return
 
         ban_embed = discord.Embed(title='Banned', description='A user have been Banned.', color=0xFFFFFF, timestamp=datetime.datetime.utcnow(), footer=user.id)
 
@@ -68,21 +110,38 @@ class Moderation(commands.Cog):
 
         ban_embed.set_footer(text=f'UserID: {user.id}')
 
-        channel = ctx.bot.get_channel(MODLOGS)
-        await channel.send(embed=ban_embed)
+        channel = ctx.bot.get_channel(modlog_channel)
 
         await ctx.guild.ban(user, reason=reason)
+
+        await channel.send(embed=ban_embed)
         await ctx.send(f'User {user.name} has been banned successfully.')
 
     @commands.command(name='unban', help='Unbans a specified user.')
     @commands.has_permissions(ban_members=True)
-    async def unban(self, ctx, id: int):
+    async def unban(self, ctx, user: commands.UserConverter=None):
 
-        if not id:
-            await ctx.send('Please provide a user\'s id.')
+        if not user:
+            await ctx.send('Please provide a user.')
             return
 
-        user = ctx.bot.get_user(id)
+        if user == ctx.author:
+            await ctx.send('You can\'t unban yourself.')
+            return
+
+        if user.id == ctx.guild.owner_id:
+            await ctx.send('You can\'t unban the server owner.')
+            return
+
+        guild = guilds_collection.find_one(filter={"guild_id": ctx.guild.id})
+        modlog_channel = guild['modlog_channel']
+
+        if not modlog_channel:
+            await ctx.send('ModLogs channel not found, Set it up using ``set_modlog_channel``')
+
+            await ctx.guild.unban(user)
+            await ctx.send(f'User {user.name} has been unbanned successfully.')
+            return
 
         unban_embed = discord.Embed(title='Unbanned', description='A user have been Unbanned.', color=0xFFFFFF, timestamp=datetime.datetime.utcnow(), footer=user.id)
 
@@ -93,14 +152,14 @@ class Moderation(commands.Cog):
 
         unban_embed.set_footer(text=f'UserID: {user.id}')
 
-        channel = ctx.bot.get_channel(MODLOGS)
+        channel = ctx.bot.get_channel(modlog_channel)
         await channel.send(embed=unban_embed)
 
         await ctx.guild.unban(user)
         await ctx.send(f'User {user.name} has been unbanned successfully.')
 
     @commands.command(name='dm', help='DM\'s a specified user.')
-    @commands.has_role(DMROLE)
+    @is_mod()
     async def dm(self, ctx, user: commands.MemberConverter=None, *, message=None):
 
         if not user:
@@ -116,7 +175,7 @@ class Moderation(commands.Cog):
         await user.dm_channel.send(message)
 
     @commands.command(name='say', help='Says Something given.')
-    @commands.has_role(SAYROLE)
+    @is_mod()
     async def say(self, ctx, *, message=None):
 
         if not message:
@@ -127,7 +186,7 @@ class Moderation(commands.Cog):
         await ctx.send(message)
 
     @commands.command(name='say_in_channel', help='Says Something given in a specified channel.')
-    @commands.has_role(SAYROLE)
+    @is_mod()
     async def say_in_channel(self, ctx, channel: commands.TextChannelConverter=None, *, message=None):
 
         if not channel:

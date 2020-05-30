10b608e8485427a40254ca716a0b676ec2e1bdc7
Syed Ahkam <smahkam57@gmail.com>
2020-05-30 15:57:07 +0530

    New Eval command

--- cogs/owner_only.py
+++ cogs/owner_only.py
@@ -17,6 +17,22 @@ db = mongo_client.jeju
 guilds_collection = db.guilds
 bot_collection = db.bot
 
+# For eval command
+def insert_returns(body):
+    # insert return stmt if the last expression is a expression statement
+    if isinstance(body[-1], ast.Expr):
+        body[-1] = ast.Return(body[-1].value)
+        ast.fix_missing_locations(body[-1])
+
+    # for if statements, we insert returns into the body and the orelse
+    if isinstance(body[-1], ast.If):
+        insert_returns(body[-1].body)
+        insert_returns(body[-1].orelse)
+
+    # for with blocks, again we insert returns into the body
+    if isinstance(body[-1], ast.With):
+        insert_returns(body[-1].body)
+
 # Main Cog Class
 class OwnerOnly(commands.Cog):
 
@@ -44,11 +60,54 @@ class OwnerOnly(commands.Cog):
         ctx.bot.load_extension(f'cogs.{extension}')
         await ctx.send(f'{extension} reloaded Successfully.')
 
-    @commands.command(name='eval', help='Evaluate a piece of code.')
+    @commands.command()
     @commands.is_owner()
-    async def _eval(self, ctx, *, code):
-        evaluated_code = str(eval(code))
-        await ctx.send(f'```{evaluated_code}```')
+    async def eval_fn(ctx, *, cmd):
+        """Evaluates input.
+        Input is interpreted as newline seperated statements.
+        If the last statement is an expression, that is the return value.
+        Usable globals:
+          - `bot`: the bot instance
+          - `discord`: the discord module
+          - `commands`: the discord.ext.commands module
+          - `ctx`: the invokation context
+          - `__import__`: the builtin `__import__` function
+        Such that `>eval 1 + 1` gives `2` as the result.
+        The following invokation will cause the bot to send the text '9'
+        to the channel of invokation and return '3' as the result of evaluating
+        >eval ```
+        a = 1 + 2
+        b = a * 2
+        await ctx.send(a + b)
+        a
+        ```
+        """
+        fn_name = "_eval_expr"
+
+        cmd = cmd.strip("` ")
+
+        # add a layer of indentation
+        cmd = "\n".join(f"    {i}" for i in cmd.splitlines())
+
+        # wrap in async def body
+        body = f"async def {fn_name}():\n{cmd}"
+
+        parsed = ast.parse(body)
+        body = parsed.body[0].body
+
+        insert_returns(body)
+
+        env = {
+            'bot': ctx.bot,
+            'discord': discord,
+            'commands': commands,
+            'ctx': ctx,
+            '__import__': __import__
+        }
+        exec(compile(parsed, filename="<ast>", mode="exec"), env)
+
+        result = (await eval(f"{fn_name}()", env))
+        await ctx.send(result)
 
     @commands.command(name='load_guilds', help='Load the guilds in the database.')
     @commands.is_owner()

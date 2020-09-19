9cd5cc9dca1b520f2086f67438fcb4882231de6b
SyedAhkam <smahkam57@gmail.com>
2020-09-20 04:17:33 +0530

    Docstrings

--- src/cogs/info.py
+++ src/cogs/info.py
@@ -2,7 +2,8 @@ from discord.ext import commands
 from utils.logger import bot_logger
 from utils.embeds import normal_embed
 
-class Info(commands.Cog):
+class Info(commands.Cog, name='info'):
+    """General commands to get info about certain things."""
     def __init__(self, bot):
         self.bot = bot
 
@@ -12,6 +13,10 @@ class Info(commands.Cog):
         brief='Check the discord ws latency.'
     )
     async def ping(self, ctx):
+        """Get the bot's latency in ms's
+        This could be referred to as the Discord WebSocket protocol latency.
+        If you're experiencing high latency then probably something's up on the discord side. https://status.discord.com
+        """
         latency = round(self.bot.latency * 1000)
         embed = normal_embed(
             ctx,

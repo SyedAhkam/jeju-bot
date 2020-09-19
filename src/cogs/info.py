8bcec11423c193505179a24684a890d1c4b9a03d
SyedAhkam <smahkam57@gmail.com>
2020-09-19 21:14:42 +0530

    Ping command

--- src/cogs/info.py
+++ src/cogs/info.py
@@ -1,15 +1,25 @@
 from discord.ext import commands
 from utils.logger import bot_logger
+from utils.embeds import normal_embed
 
 class Info(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(enabled=True, ignore_extra=False)
-    @commands.cooldown(5, 10, commands.BucketType.user)
-    async def test(self, ctx, argument1: int, argument2):
-        bot_logger.info('OK')
-        await ctx.send('everything ok')
+    @commands.command(
+        name='ping',
+        aliases=['p', 'latency'],
+        brief='Check the discord ws latency.'
+    )
+    async def ping(self, ctx):
+        latency = round(self.bot.latency * 1000)
+        embed = normal_embed(
+            ctx,
+            title='Pong!',
+            description=f"""Hello, I currently am experiencing a latency of ``{latency}ms``.
+            If you\'re experiencing high latency then probably something\'s up on the discord side."""
+        )
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Info(bot))

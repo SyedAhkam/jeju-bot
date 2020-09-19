11959513db597d13fe46ed68462359a453cb490f
SyedAhkam <smahkam57@gmail.com>
2020-09-19 19:13:48 +0530

    Error handler

--- src/cogs/info.py
+++ src/cogs/info.py
@@ -5,8 +5,9 @@ class Info(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def test(self, ctx):
+    @commands.command(enabled=True, ignore_extra=False)
+    @commands.cooldown(5, 10, commands.BucketType.user)
+    async def test(self, ctx, argument1: int, argument2):
         bot_logger.info('OK')
         await ctx.send('everything ok')
 

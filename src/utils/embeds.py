9dbb71194265c605915111f111513c0d18c924ee
SyedAhkam <smahkam57@gmail.com>
2020-09-20 04:17:20 +0530

    Added timestamps to embeds

--- src/utils/embeds.py
+++ src/utils/embeds.py
@@ -8,12 +8,22 @@ colors = {
 
 def normal_embed(ctx, title=Embed.Empty, description=Embed.Empty):
     """An embed for common use"""
-    embed = Embed(description=description, color=colors['normal'])
+    embed = Embed(
+        description=description,
+        color=colors['normal'],
+        timestamp=datetime.utcnow()
+    )
     embed.set_author(name=title, icon_url=ctx.bot.user.avatar_url)
+    embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
     return embed
 
 def error_embed(ctx, error_name=Embed.Empty, error_msg=Embed.Empty):
     """An embed for use when handling errors"""
-    embed = Embed(description=error_msg, color=colors['error'])
+    embed = Embed(
+        description=error_msg,
+        color=colors['error'],
+        timestamp=datetime.utcnow()
+    )
     embed.set_author(name=error_name, icon_url=ctx.bot.user.avatar_url)
+    embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
     return embed

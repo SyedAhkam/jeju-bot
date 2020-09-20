15d66befc7869d10cc30483ed0ac7c9fa518f861
SyedAhkam <smahkam57@gmail.com>
2020-09-21 04:13:47 +0530

    Blacklist system

--- src/utils/db.py
+++ src/utils/db.py
@@ -1,3 +1,7 @@
+async def is_document_exists(collection, id):
+    """Determine if a document with a specific id exist in a collection or not"""
+    return await collection.count_documents({'_id': id}, limit=1)
+
 async def get_guild(guilds_collection, guild_id):
     """Get a guild from db."""
     return await guilds_collection.find_one({'_id': guild_id})

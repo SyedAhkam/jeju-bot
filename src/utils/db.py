26ecc6e24866f79b3be58e8e87c6cd44aa2370b7
SyedAhkam <smahkam57@gmail.com>
2020-10-09 22:10:05 +0530

    Setup logging

--- src/utils/db.py
+++ src/utils/db.py
@@ -2,9 +2,11 @@ async def is_document_exists(collection, id):
     """Determine if a document with a specific id exist in a collection or not"""
     return await collection.count_documents({'_id': id}, limit=1)
 
+
 async def get_guild(guilds_collection, guild_id):
     """Get a guild from db."""
     return await guilds_collection.find_one({'_id': guild_id})
+
 
 async def add_guild(guilds_collection, guild):
     """Add a guild to db."""
@@ -15,15 +17,18 @@ async def add_guild(guilds_collection, guild):
         'bot_prefix': '+'
     })
 
+
 async def remove_guild(guilds_collection, guild_id):
     """Remove a guild from db."""
     await guilds_collection.delete_one({'_id': guild_id})
+
 
 async def get_custom_prefix(guilds_collection, guild_id):
     """Gets a custom prefix from guild document of a guild."""
     doc = await get_guild(guilds_collection, guild_id)
 
     return doc['bot_prefix']
+
 
 async def set_config_value(config_collection, guild_id, config_type, config_value):
     """Set a config value for a specific type and guild."""
@@ -33,12 +38,14 @@ async def set_config_value(config_collection, guild_id, config_type, config_valu
         'config_value': config_value
     })
 
+
 async def delete_config_value(config_collection, guild_id, config_type):
     """Delete a config value for a specific type and guild."""
     await config_collection.delete_one({
         'guild_id': guild_id,
         'config_type': config_type
     })
+
 
 async def delete_guild_config_values(config_collection, guild_id):
     """Delete all the config values associated to a guild."""
@@ -52,6 +59,7 @@ async def update_config_value(config_collection, guild_id, config_type, config_v
         {'$set': {'config_value': config_value}}
     )
 
+
 async def is_config_value_set(config_collection, guild_id, config_type):
     """Check if a config_value is set or not."""
     return await config_collection.count_documents(
@@ -59,11 +67,13 @@ async def is_config_value_set(config_collection, guild_id, config_type):
         limit=1
     )
 
+
 async def get_config_value(config_collection, guild_id, config_type):
     """Get a config value for a specific type and guild."""
     doc = await config_collection.find_one({
         'guild_id': guild_id,
         'config_type': config_type
     })
-
+    if not doc:
+        return None
     return doc['config_value']
